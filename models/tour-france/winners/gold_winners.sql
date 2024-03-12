cad6c6ac862a627c88d20bee521f8ef784b3ce2e
Freire Business <freirebusiness@outlook.com>
2024-03-12 22:55:08 +0000

    refactor: alteração modelagem alteração chave primária e estrangeira para melhor modelagem através da gold_winners

--- models/tour-france/winners/gold_winners.sql
+++ models/tour-france/winners/gold_winners.sql
@@ -1,6 +1,11 @@
 select 
-    row_number() over(order by ano) id_winner,
-    *,
-    datediff(year, born, current_date) idade
+    row_number() over(order by w.ano) id_winner,
+    w.*,
+    datediff(year, w.born, current_date) idade,
+    t.id_tour as tour_id
 from 
-    {{ ref('silver_winners') }}
+    {{ ref('silver_winners') }} w
+    left join 
+        {{ ref('gold_tours') }} t
+    on
+    w.ano = t.ano

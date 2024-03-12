5303ba4fc0dce36cc96dc20709104867aef565fe
Freire Business <freirebusiness@outlook.com>
2024-03-12 20:11:58 +0000

    fix: correção no código do silver_finishers

--- models/tour-france/finishers/silver_finishers.sql
+++ models/tour-france/finishers/silver_finishers.sql
@@ -7,7 +7,7 @@ with
             time as duracao,
             team
         FROM 
-            {{ ref('tb_finishers') }}
+            tb_finishers
     ),
 
     base_trat as (
@@ -15,7 +15,7 @@ with
             ano,
             classificacao,
             split_part(rider, '(', 1) as rider,
-            replace(duracao, '+ ', '') duracao,
+            replace(replace(replace(duracao, '+ ', ''), '+', ''),'′', '''') duracao,
             team
         from    
             base
@@ -23,12 +23,35 @@ with
 
 select
     cast(ano as integer) ano,
-    cast(classificacao as integer) classificacao,
+    cast(replace(replace(classificacao, 'DSQ', 0), '', 0) as integer) classificacao,
     rider,
     nullif(duracao, '') duracao,
-    cast(nullif(split_part(duracao, 'h', 1), '') as integer) as horas,
-    cast(nullif(replace(split_part(duracao, ' ', 2), '''', ''), '') as integer) as minutos,
-    cast(nullif(replace(split_part(duracao, ' ', 3), '"', ''), '') as integer) as segundos,
+    cast(case
+        when position('h' in duracao) = 2 then split_part(duracao, 'h', 1)
+        when position('h' in duracao) = 3 then split_part(duracao, 'h', 1)
+        when position('h' in duracao) = 4 then split_part(duracao, 'h', 1)
+    end as integer) horas,
+    cast(
+        case
+            when nullif(duracao, '') is null then null
+            when position('h' in duracao) = 2 then split_part(replace(duracao, '''', ' '), ' ', 2)
+            when position('h' in duracao) = 3 then split_part(replace(duracao, '''', ' '), ' ', 2)
+            when position('h' in duracao) = 4 then split_part(replace(duracao, '''', ' '), ' ', 2)
+            when position('''' in duracao) = 2 then split_part(duracao, '''', 1)
+            when position('''' in duracao) = 3 then split_part(duracao, '''', 1)
+            when position('''' in duracao) = 4 then split_part(duracao, '''', 1)
+        end
+    as integer) minutos,
+    case
+        when replace(split_part(duracao, ' ', 3), '"', '') != '' then replace(replace(split_part(duracao, ' ', 3), '"', ''), '''', '')
+        when position('h' in duracao) = 2 then split_part(replace(replace(duracao, '"', ' '), '''', ' '), ' ', 3)
+        when position('h' in duracao) = 3 then split_part(replace(replace(duracao, '"', ' '), '''', ' '), ' ', 3)
+        when position('''' in duracao) = 4 then split_part(replace(replace(duracao, '''', ''), '"', ' '), ' ', 2)
+        when position('"' in duracao) = 2 then split_part(duracao, '"', 1)
+        when position('"' in duracao) = 3 then split_part(duracao, '"', 1)
+        when position('''' in duracao) = 2 then split_part(replace(duracao, '"', ' '), ' ', 2)
+        when position('''' in duracao) = 3 then split_part(replace(duracao, '"', ' '), ' ', 2)
+    end segundos,
     nullif(team, '') team
 from
     base_trat

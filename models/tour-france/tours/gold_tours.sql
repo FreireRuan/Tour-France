a0d891765880e7fb8e680054e7dab8c4971dbbf7
Freire Business <freirebusiness@outlook.com>
2024-03-12 22:52:17 +0000

    refactor: alteração modelagem alteração chave primária e estrangeira para melhor modelagem através da gold_tours

--- models/tour-france/tours/gold_tours.sql
+++ models/tour-france/tours/gold_tours.sql
@@ -1,10 +1,5 @@
 select
     row_number() over() as id_tour,
-    t.*,
-    s.id_winner winner_id
+    t.*
 from 
     {{ ref('silver_tours') }} t
-    left join
-        {{ ref('gold_winners') }} s 
-        on 
-        t.ano = s.ano

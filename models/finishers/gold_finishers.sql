99d0715e98bd03afc4fb7089f5171919eee2ca1f
Freire Business <freirebusiness@outlook.com>
2024-03-13 23:15:01 +0000

    chore: alteração na estrutura de pastas

---
+++ models/finishers/gold_finishers.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+    row_number() over() id_finishers,
+    f.*,
+    t.id_tour as tour_id
+from
+    {{ ref('silver_finishers') }} f
+    left join
+        {{ ref('gold_tours') }} t
+        on
+        f.ano = t.ano
